0df3f280ac57babff47e37863acaf25b8e6621f7
Gustavo Caris <gustavocaris14052002@gmail.com>
2026-05-30 20:06:24 -0300

    feat: create request w/ get

--- Server/server.js
+++ Server/server.js
@@ -1,1 +1,21 @@
 import express from 'express';
+
+const app = express();
+
+app.get('/users', (req, res) => {
+    res.send('List of users');
+})
+
+app.listen(3000)
+
+
+/*
+
+we routers need:
+
+- router type/ w/ params http
+- endpoints
+
+-www.servidor.com/api/v1/endpoint
+
+*/
