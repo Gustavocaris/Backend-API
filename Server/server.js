9ea3c046adcabebafb571fe9eb4f4ba8444715a7
Gustavo Caris <gustavocaris14052002@gmail.com>
2026-05-31 15:57:52 -0300

    feat: create const users

--- Server/server.js
+++ Server/server.js
@@ -2,6 +2,14 @@ import express from 'express';
 
 const app = express();
 
+const users = []
+
+app.post('/users', (req, res) => {
+    const user = req.body;
+    users.push(user);
+    res.send('User created');
+}
+
 app.get('/users', (req, res) => {
     res.send('List of users');
 })
